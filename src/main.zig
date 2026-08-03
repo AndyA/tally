651d876c72623e0af86acfa60d21f5cce5e99fee
Andy Armstrong <andy@hexten.net>
2026-08-03 15:40:46 +0100

    Run locally.

--- src/main.zig
+++ src/main.zig
@@ -9,7 +9,7 @@ const MESSAGE = "OK, ready, let's do it.  ";
 
 pub fn main(init: std.process.Init) !void {
     var w_buf: [32]u8 = undefined;
-    var dev = try Io.Dir.cwd().openFile(init.io, "/dev/serial0", .{ .mode = .read_write });
+    var dev = try Io.Dir.cwd().openFile(init.io, "/dev/ttyUSB0", .{ .mode = .read_write });
     defer dev.close(init.io);
     var w = dev.writer(init.io, &w_buf);
 
@@ -28,8 +28,7 @@ pub fn main(init: std.process.Init) !void {
             _ = try w.interface.writeByte(0x30 | (@as(u8, @intCast(pos)) & 0x0f));
             _ = try w.interface.write(msg[pos..][0..16]);
             try w.interface.flush();
-            print("{s}\n", .{msg[pos..][0..16]});
-            try init.io.sleep(.fromMilliseconds(250), .awake);
+            try init.io.sleep(.fromMilliseconds(50), .awake);
         }
     }
 }
